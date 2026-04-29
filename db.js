09c8f66559488ba1c1970056f742922e9649db13
areeba <mahzalakhan07@gmail.com>
2026-04-29 16:41:40 +0500

    fixed db connection

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.db.eajmdrghdoykrechzbyr.supabase.co,
-  port: process.env.DB_PORT || 5432,
-  user: process.env.postgres,
-  password: process.env.veniatvidiat34,
-  database: process.env.postgres,
+  host: 'db.eajmdrghdoykrechzbyr.supabase.co',
+  port: 5432,
+  user: 'postgres',
+  password: 'veniatvidiat34',
+  database: 'postgres',
   ssl: { rejectUnauthorized: false }
 });
 
@@ -17,12 +17,8 @@ pool.connect((err) => {
   }
 });
 
-// This makes it work like mysql2 so you don't need to change server.js
-pool.query = pool.query.bind(pool);
-
 module.exports = {
   query: (sql, params, callback) => {
-    // Convert MySQL ? placeholders to PostgreSQL $1, $2...
     let i = 0;
     const pgSql = sql.replace(/\?/g, () => `$${++i}`);
     pool.query(pgSql, params, (err, result) => {
